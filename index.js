ed4aebf4ca850045de4ce2dbcb26da55ae0484c0
Shrey5908 <112169483+Shrey5908@users.noreply.github.com>
2023-10-09 16:31:11 +0530

    Re-Build the Whole Site

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+require('dotenv').config();
 const express = require("express");
-const path = require("path");
 const studentrouter = require("./routes/student");
 const { connectMongoDB } = require("./connection");
 const { log } = require("./middleware");
 
 const app = express();
 const PORT = 8000;
+const MONGODB = process.env.MONGODB;
 
-connectMongoDB("mongodb://127.0.0.1:27017/project").then(() => {
+connectMongoDB(MONGODB).then(() => {
   console.log("MongoDB Connected! ");
 });
 
@@ -17,6 +18,11 @@ connectMongoDB("mongodb://127.0.0.1:27017/project").then(() => {
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(log("log.txt"));
+// app.use("/student", studentrouter);
 app.use("/student", studentrouter);
+
+app.get('/',(req,res)=>{
+  res.redirect('/student');
+})
 
 app.listen(PORT, () => console.log(`Server Started! at PORT: ${PORT}`));
